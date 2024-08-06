81c958a23b485b3acf5b84d40cdec10c393e67b7
codecarftwt <codecarftwt@gmail.com>
2024-08-06 18:27:05 +0530

    update  wishlist

--- src/Screens/Ready To go see All/SeeAll.js
+++ src/Screens/Ready To go see All/SeeAll.js
@@ -1,140 +1,112 @@
-import {
-  FlatList,
-  Image,
-  Pressable,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
-import React, {useEffect, useLayoutEffect, useState} from 'react';
-import {useDispatch, useSelector} from 'react-redux';
-import {fetchRedyToGo} from '../../Redux/Slice/ready_to_go';
-import SkeletonLoader from '../../Components/Loader/SkeletonLoader';
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, Pressable, View, FlatList} from 'react-native';
+
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from 'react-native-responsive-screen';
-import {useNavigation} from '@react-navigation/native';
-import {Images} from '../../Constants';
-import {NoImg} from '../../Constants/Icons';
 import {globalColors} from '../../Assets/Theme/globalColors';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {Get, Post, Remove} from '../../Services/Get';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {fetchProducts} from '../../Redux/Slice/productSlice';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchWishlist} from '../../Redux/Slice/wishlistSlice';
 import {getToken} from '../../Utils/localstorage';
+import {SafeAreaView} from 'react-native';
+import {fetchRedyToGo} from '../../Redux/Slice/ready_to_go';
+import Readytogo from '../../Components/Ready To Go/Readytogo';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+
 const SeeAll = () => {
+  const navigation = useNavigation();
+
   const dispatch = useDispatch();
+  const {categories, categoryStatus, categoryError} = useSelector(
+    state => state.category,
+  );
   const {redytogoProducts, redytogoStatus, redytogoError} = useSelector(
     state => state.redytogo,
   );
-  const [savedItems, setSavedItems] = useState({});
-  const [token, setToken] = useState();
-  const navigation = useNavigation();
-  const [wishlistdata, setWishlist] = useState();
-  useEffect(() => {
-    // Synchronize savedItems state with wishlistdata when it changes
-    const updatedSavedItems = {};
-    wishlistdata?.Wishlist.forEach(item => {
-      updatedSavedItems[item.id] = true;
-    });
-    setSavedItems(updatedSavedItems);
-  }, [wishlistdata]);
+  const {products, status, error} = useSelector(state => state.product);
+  const {items, loading: wishlistLoading} = useSelector(
+    state => state.wishlist,
+  );
+  const [tokenData, setTokenData] = useState(null);
+  const [wishlist, setWishlist] = useState([]);
 
   useEffect(() => {
-    const getwishlist = async () => {
-      const token = await getToken();
-      setToken(token);
-      const data = await Get(token);
-      setWishlist(data);
-    };
-    getwishlist();
-  }, []);
-
-  useLayoutEffect(() => {
+    data();
     dispatch(fetchRedyToGo());
+    dispatch(fetchProducts());
   }, [dispatch]);
 
-  const toggleSave = async itemId => {
-  
-    setSavedItems(prev => {
-      const isSaved = !!prev[itemId];
-      const newSavedItems = {
-        ...prev,
-        [itemId]: !isSaved,
-      };
-      if (!isSaved) {
-        Post({product_id: itemId}, token);
-      } else {
-        Remove({product_id: itemId}, token);
+  useFocusEffect(
+    React.useCallback(() => {
+      data();
+      dispatch(fetchProducts());
+    }, [dispatch, tokenData]),
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await getToken();
+        if (token) {
+          setTokenData(token);
+          await dispatch(fetchWishlist(token));
+        }
+      } catch (error) {
+        console.log('Error retrieving data:', error);
       }
-      return newSavedItems;
-    });
-    const data = await Get(token);
-    setWishlist(data);
+    };
+    fetchData();
+  }, [dispatch, getToken]);
+  const data = () => {
+    if (items.Wishlist) {
+      const itemIdList = items.Wishlist?.map(item => ({id: item}));
+      const productIds = new Set(itemIdList.map(item => Number(item.id)));
+      const result = products.map(productItem => ({
+        ...productItem,
+        isWatchList: productIds.has(productItem.id),
+      }));
+      setWishlist(result);
+    } else if (wishlist) {
+      setWishlist(products);
+    }
   };
+
+  useEffect(() => {
+    data();
+  }, [items, products, categories, tokenData]);
 
   return (
     <GestureHandlerRootView>
       <SafeAreaView>
-        <View>
-          <View style={styles.maincontainer}>
-            <FlatList
-              data={redytogoProducts}
-              renderItem={({item}) => (
-                <Pressable
-                  onPress={() =>
-                    navigation.navigate('ProductDetail', {userId: item.id})
-                  }>
-                  {redytogoStatus === 'loading' ? (
-                    <View style={{marginLeft: '2.5%'}}>
-                      <SkeletonLoader count={6} />
-                    </View>
-                  ) : (
-                    <View style={styles.container}>
-                      <View>
-                        {item?.images[0] ? (
-                          <View>
-                            <Image
-                              style={styles.image}
-                              source={{
-                                uri: item.images[0],
-                              }}
-                              resizeMode="cover"
-                            />
-                          </View>
-                        ) : (
-                          <Image
-                            source={NoImg}
-                            style={styles.image}
-                            resizeMode="contain"
-                          />
-                        )}
-                        <Pressable
-                          onPress={() => toggleSave(item.id)}
-                          style={styles.saveImagea}>
-                          <Image
-                            style={styles.saveImage}
-                            source={
-                              savedItems[item.id]
-                                ? Images.saveIconFill
-                                : Images.saveIconUnFill
-                            }
-                          />
-                        </Pressable>
-                      </View>
-
-                      <View style={styles.detailsContainer}>
-                        <Text style={styles.name}>{item.name}</Text>
-                        <Text style={styles.price}>{item.price} AED</Text>
-                      </View>
-                    </View>
-                  )}
-                </Pressable>
-              )}
-              keyExtractor={item => item.id}
-              numColumns={2}
-            />
-          </View>
+        <View style={styles.maincontainer}>
+          <FlatList
+            data={redytogoProducts}
+            renderItem={({item}) => (
+              <Pressable
+                key={item.id}
+                onPress={() =>
+                  navigation.navigate('ProductDetail', {
+                    userId: item.id,
+                  })
+                }>
+                <Readytogo
+                  key={item?.id}
+                  id={item?.id}
+                  uri={item?.images[0]}
+                  name={item?.name}
+                  price={item?.price}
+                  saved={item?.saved}
+                  product_id={item?.id}
+                  isWatchList={item?.isWatchList}
+                />
+              </Pressable>
+            )}
+            keyExtractor={item => item.id}
+            numColumns={2}
+          />
         </View>
       </SafeAreaView>
     </GestureHandlerRootView>
@@ -146,23 +118,20 @@ export default SeeAll;
 const styles = StyleSheet.create({
   maincontainer: {
     paddingHorizontal: wp('1%'),
-    marginTop: hp('5%'),
+    marginTop: hp('10%'),
     flexDirection: 'row',
   },
   name: {
     fontFamily: 'Product Sans',
     color: globalColors.black,
-    // fontWeight: '600'
     marginBottom: hp('0.5%'),
     fontSize: 17,
   },
-
   image: {
     borderRadius: 6,
     width: wp('46%'),
     height: hp('21%'),
   },
-
   saveImagea: {
     position: 'absolute',
     marginTop: wp('0.1%'),
@@ -189,9 +158,8 @@ const styles = StyleSheet.create({
   },
   price: {
     fontSize: 17,
-    fontWeight: '400',
+    fontWeight: '700',
     fontFamily: 'Product Sans',
     color: globalColors.black,
-    fontWeight: '700',
   },
 });
